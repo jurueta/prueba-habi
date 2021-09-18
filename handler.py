810fb84716aeb9608b0b28ba9ccb3dcfac98a0ba
Jesus Urueta <juruetacantillo@gmail.com>
2021-09-18 01:37:04 -0500

    Cambios en los handlers

--- handler.py
+++ handler.py
@@ -3,17 +3,18 @@ from mysql.connector import connect
 from mysql.connector.errors import ProgrammingError
 import os
 
+
 def connect_db():
     """
         Create conecction with the DataBase With credencials provide in .env
     """
     try:
         conection = connect(
-            host = os.getenv("HOSTDB", "3.130.126.210"),
-            port = os.getenv("PORTDB", "3309"),
-            user = os.getenv("USERDB", "pruebas"),
-            passwd = os.getenv("PASSWDDB", "VGbt3Day5R"),
-            database = os.getenv("DATABASE", "habi_db")
+            host=os.getenv("HOSTDB", "3.130.126.210"),
+            port=os.getenv("PORTDB", "3309"),
+            user=os.getenv("USERDB", "pruebas"),
+            passwd=os.getenv("PASSWDDB", "VGbt3Day5R"),
+            database=os.getenv("DATABASE", "habi_db")
         )
         return conection
     except Exception as error:
@@ -33,16 +34,17 @@ def property(event, context):
     except Exception:
         return response_api(500, {"message": "Error to connect with database"})
 
-    query_property = """SELECT p.address, p.city, p.price, p.description, p.year, s.name as status
-        FROM property p
-        INNER JOIN status_history sh ON sh.id = (SELECT id 
-        FROM status_history 
-        WHERE property_id = p.id 
-        ORDER BY update_date 
-        DESC LIMIT 1)
-        INNER JOIN status s ON s.id = sh.status_id
-        WHERE sh.status_id IN (3,4,5) """
-    
+    query_property = """SELECT p.address, p.city, p.price, p.description,
+            p.year, s.name as status
+            FROM property p
+            INNER JOIN status_history sh ON sh.id = (SELECT id
+            FROM status_history
+            WHERE property_id = p.id
+            ORDER BY update_date
+            DESC LIMIT 1)
+            INNER JOIN status s ON s.id = sh.status_id
+            WHERE sh.status_id IN (3,4,5)"""
+
     # filters
     if event["multiValueQueryStringParameters"]:
         params = event["multiValueQueryStringParameters"]
@@ -55,8 +57,8 @@ def property(event, context):
 
         # List comprehension of filters
         query_conditions = " ".join([
-            filters_data[param[0]].format(param[1][0]) 
-            for param in params.items() 
+            filters_data[param[0]].format(param[1][0])
+            for param in params.items()
             if param[0] in filters_data.keys()
             ])
 
@@ -64,8 +66,8 @@ def property(event, context):
 
     # Show response
     try:
-      cursor.execute(query_property)
-      result_property = cursor.fetchall()
+        cursor.execute(query_property)
+        result_property = cursor.fetchall()
     except ProgrammingError:
         return response_api(400, {"message": "Check the filters"})
 
@@ -73,8 +75,8 @@ def property(event, context):
 
 
 def response_api(code, body):
-    """ 
-        Render response by code and body 
+    """
+        Render response by code and body
     """
     response = {
         "statusCode": code,
